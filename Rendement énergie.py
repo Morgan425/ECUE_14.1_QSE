c2ca933f60b9c5fc9f2d6a9e451caf3a4080b20a
Morgan Aubert <morganaubert425@gmail.com>
2024-10-18 16:48:41 +0200

    single test

--- Rendement énergie.py
+++ Rendement énergie.py
@@ -6,8 +6,8 @@ import openpyxl as xl
 charge_efficiency=0.9
 sell_efficiency=0.9
 
-sell_price=100
-buy_price=40
+sell_price=150
+buy_price=20
 
 storage_capacity=10  # MWh
 charge_flux=1  
@@ -36,19 +36,24 @@ for t in range(1,temps):
     elif price>sell_price:
         selled_energy=min(sell_flux,Storage[t-1])
         Storage[t]=Storage[t-1]-selled_energy
-        Benefit[t]=Benefit[t-1]+(sell_flux*sell_efficiency*price)
+        Benefit[t]=Benefit[t-1]+(sell_flux*selled_energy*price)
     
     else:
         Benefit[t]=Benefit[t-1]
         Storage[t]=Storage[t-1]
 
-print(f'Potential benefit (if all remaining energy sold): {round(Benefit[-1]+Storage[-1]*sell_efficiency*Energy_Price[-1],2)}€')
+potential_benefit=round(Benefit[-1]+Storage[-1]*sell_efficiency*Energy_Price[-1],2)
+print(f'Potential benefit (if all remaining energy sold): {potential_benefit}€')
 
 fig,(ax1,ax2,ax3)=plt.subplots(3,1,sharex=True)
 ax2.plot(Storage)
 ax2.set_title('Storage')
-ax1.plot(Benefit)
+
+ax1.plot(Benefit,'g')
 ax1.set_title('Benefit')
+ax1.axhline(0,color='black')
+ax1.scatter(len(Energy_Price),potential_benefit,color='g')
+
 ax3.plot(Energy_Price,'r')
 ax3.set_title('Energy_Price')
 plt.show()
